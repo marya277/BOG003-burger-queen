3a23deb0145720a0bd69a8f2bae52b326b863b99
Lorraine Gelis <lgelisdiaz@outlook.com>
2021-10-26 15:56:55 -0500

    Se realiza componente vistaMesero y el llamado de la data menú

---
+++ src/data/desayuno.js
@@ -0,0 +1,15 @@
+import cafeAmericano from '../assets/img/americanCoffee.png'
+import cafeConLeche from '../assets/img/cafeLeche.png';
+import sandwich from '../assets/img/sandwich.png'
+import jugoFruta from '../assets/img/jugos.png'
+
+const breakfast = {
+    items: [
+      { id: 1, title: "Café americano", image: cafeAmericano, cost: 2000 },
+      { id: 2, title: "Café con leche", image: cafeConLeche, cost: 3000 },
+      { id: 3, title: "Sandwich con jamón y queso", image: sandwich, cost: 12500 },
+      { id: 4, title: "Jugo de frutas naturales", image: jugoFruta, cost: 2500 },
+    ],
+    };
+    
+    export default breakfast;
