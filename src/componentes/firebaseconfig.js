151fbbb25418e83b788e618948cb44d3e5f0a96a
Giselle <kgiselle17@gmail.com>
2021-11-02 16:00:42 -0500

    configuración con Firebase y firestore

--- src/componentes/firebaseconfig.js
+++ src/componentes/firebaseconfig.js
@@ -1,18 +1,23 @@
-/* import firebase from 'firebase/app'
-import 'firebase/firestore'
+import { initializeApp } from "firebase/app";
 
+import { getFirestore } from "firebase/firestore"
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDqiVJSRt84xRekRung9p0O7-IEKHDKEdg",
-    authDomain: "burguerqueen-krustykrab.firebaseapp.com",
-    projectId: "burguerqueen-krustykrab",
-    storageBucket: "burguerqueen-krustykrab.appspot.com",
-    messagingSenderId: "539017906516",
-    appId: "1:539017906516:web:575c1123e8eacfed1fa0c1",
-    measurementId: "G-3ZM0YRNZ7N"
-  };
-  
-  // Initialize Firebase
-  const fb = firebase.initializeApp(firebaseConfig);
-  const db = fb.firestore()
+  apiKey: "AIzaSyDqiVJSRt84xRekRung9p0O7-IEKHDKEdg",
+  authDomain: "burguerqueen-krustykrab.firebaseapp.com",
+  projectId: "burguerqueen-krustykrab",
+  storageBucket: "burguerqueen-krustykrab.appspot.com",
+  messagingSenderId: "539017906516",
+  appId: "1:539017906516:web:575c1123e8eacfed1fa0c1",
+  measurementId: "G-3ZM0YRNZ7N"
+};
 
-  export default db; */
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export default db;
