8b9e566f61c0a82b1098181e7ed095d278f0bd25
Giselle <kgiselle17@gmail.com>
2021-11-11 17:50:29 -0500

    Se quitaron espacios y se dio formato al codigo

--- src/componentes/VistaMesero/ProductsContainerMenu.jsx
+++ src/componentes/VistaMesero/ProductsContainerMenu.jsx
@@ -18,39 +18,39 @@ const ProductsContainer = ({selectProduct, state, handleQuantity }) => {
     return(
         <section className="container-fluid">
             <div className="row">
- 
-             <ul className="nav nav-pills" id="pills-tab" role="tablist">
-                 <li className="nav-item" role="presentation">
-                     <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" onClick={() => productsType("Desayuno")}>Desayuno</button>
-                 </li>
-                 <li className="nav-item" role="presentation">
-                     <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false" onClick={() => productsType("Hamburguesas")}>Hamburguesas</button>
-                 </li>
-                 <li className="nav-item" role="presentation">
-                     <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" onClick={() => productsType("Acompañamientos")}>Acompañamientos</button>
-                 </li>
-                 <li className="nav-item" role="presentation">
-                     <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" onClick={() => productsType("Bebidas")}>Bebidas</button>
-                 </li>
-             </ul>
-             <ul>
-             </ul>
+
+            <ul className="nav nav-pills" id="pills-tab" role="tablist">
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" onClick={() => productsType("Desayuno")}>Desayuno</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false" onClick={() => productsType("Hamburguesas")}>Hamburguesas</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" onClick={() => productsType("Acompañamientos")}>Acompañamientos</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" onClick={() => productsType("Bebidas")}>Bebidas</button>
+                </li>
+            </ul>
+            <ul>
+            </ul>
  
             </div>
             
-             <ul className="contenerdorMenu">
-                 {products.map((product) => (
-                     <Product
-                         product={product}
-                         selectProduct={selectProduct}
-                         key={product.id}
-                         state={state}
-                         handleQuantity={handleQuantity} />
-                 ))}
-             </ul>
-         </section>
-     )
- }
- 
- export default ProductsContainer;
+            <ul className="contenerdorMenu">
+                {products.map((product) => (
+                    <Product
+                        product={product}
+                        selectProduct={selectProduct}
+                        key={product.id}
+                        state={state}
+                        handleQuantity={handleQuantity} />
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+export default ProductsContainer;
 
